5988f436aeabbfcaf409b29aef5ee6cc5c090e6f
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-08-31 09:47:44 -0300

    Implementando o getLivros (lista de livros)

--- src/main/java/br/com/zupacademy/vinicius/casadocodigo/livro/LivroController.java
+++ src/main/java/br/com/zupacademy/vinicius/casadocodigo/livro/LivroController.java
@@ -1,15 +1,15 @@
 package br.com.zupacademy.vinicius.casadocodigo.livro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/livros")
@@ -18,12 +18,21 @@ public class LivroController {
     @PersistenceContext
     private EntityManager manager;
 
+    @Autowired
+    private LivroRepository repository;
+
     @PostMapping
     @Transactional
     public ResponseEntity<?> salvaLivro(@RequestBody @Valid LivroForm form) {
         Livro livro = form.toModel(manager);
         manager.persist(livro);
-        LivroDto dto = new LivroDto(livro);
-        return ResponseEntity.ok().body(dto.toString());
+        return ResponseEntity.ok().body(new LivroDto(livro).toString());
+    }
+
+    @GetMapping
+    public List<LivroDto> listaLivros() {
+        List<Livro>livros = repository.findAll();
+        return livros.stream().map(livro ->
+                new LivroDto(livro)).collect(Collectors.toList());
     }
 }
--- src/main/java/br/com/zupacademy/vinicius/casadocodigo/livro/LivroDto.java
+++ src/main/java/br/com/zupacademy/vinicius/casadocodigo/livro/LivroDto.java
@@ -1,70 +1,22 @@
 package br.com.zupacademy.vinicius.casadocodigo.livro;
 
-import java.time.LocalDate;
-
 public class LivroDto {
 
-    private String titulo;
+    final Long id;
 
-    private String resumo;
-
-    private String sumario;
-
-    private Float preco;
-
-    private Integer numeroPaginas;
-
-    private Long isbn;
-
-    private LocalDate dataPublicacao;
+    final String titulo;
 
 
     public LivroDto(Livro livro) {
+        this.id = livro.getId();
         this.titulo = livro.getTitulo();
-        this.resumo = livro.getResumo();
-        this.preco = livro.getPreco();
-        this.numeroPaginas = livro.getNumeroPaginas();
-        this.isbn = livro.getIsbn();
-        this.dataPublicacao = livro.getDataPublicacao();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitulo() {
         return titulo;
-    }
-
-    public String getResumo() {
-        return resumo;
-    }
-
-    public String getSumario() {
-        return sumario;
-    }
-
-    public Float getPreco() {
-        return preco;
-    }
-
-    public Integer getNumeroPaginas() {
-        return numeroPaginas;
-    }
-
-    public Long getIsbn() {
-        return isbn;
-    }
-
-    public LocalDate getDataPublicacao() {
-        return dataPublicacao;
-    }
-
-    @Override
-    public String toString() {
-        return "LivroDto: " +
-                "\n\ttitulo= " +titulo+
-                "\n\tresumo= " +resumo+
-                "\n\tsumario= " +sumario+
-                "\n\tpreco= " +preco+
-                "\n\tnumeroPaginas= " +numeroPaginas+
-                "\n\tisbn= " +isbn+
-                "\n\tdataPublicacao= " + dataPublicacao;
     }
 }
